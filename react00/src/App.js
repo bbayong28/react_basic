ac498343cc7f351ed2f9e5fc006c8d50225c21ab
bbayong28 <ghkdud5795@hanmail.net>
2022-07-25 12:21:45 +0900

    20220725

---
+++ react00/src/App.js
@@ -0,0 +1,29 @@
+/* function App() { 
+  return <h1>제목1</h1>
+} */
+
+import Test from "./test";
+import Nav from "./Nav"; 
+
+const App = () => { 
+  //console.log('하하하하하')
+  return (
+    <>
+      <Test />
+      <Test />
+      <Test />
+      <Test />
+
+      <h1>제목1</h1>
+      <Nav />
+      <Test />
+      <Test />
+      {/* <h2>hahahaha</h2> */}
+
+    </>
+  )
+ 
+}
+
+
+export default App;
---
+++ react00/src/test.js
@@ -0,0 +1,13 @@
+/* const Test = () => { 
+
+} */
+function Test() { 
+    return (
+        <>
+            <i className="xi-bars"></i>
+            <p>Lorem ipsum dolor sit.</p>
+        </>
+    )
+}
+
+export default Test;
